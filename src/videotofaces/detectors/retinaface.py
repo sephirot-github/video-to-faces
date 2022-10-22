6b2b3de160426644e552ae515c455dda6bb7d1f3
nsndp <zagumennov.da@gmail.com>
2022-10-22 17:38:28 +0000

    retinaface wip

--- src/videotofaces/detectors/retinaface.py
+++ src/videotofaces/detectors/retinaface.py
@@ -18,18 +18,22 @@ from ..utils import bbox
 
 
 class FPN(nn.Module):
-    def __init__(self, cins, cout, relu):
+    def __init__(self, cins, cout, relu, extra_level):
         super(FPN, self).__init__()
         self.outputs = nn.ModuleList([ConvUnit(cin, cout, 1, 1, 0, relu) for cin in cins])
+        if extra_level: # corresponds to "P6" in the paper
+            self.extra = ConvUnit(cins[-1], cout, 3, 2, 1, relu)
         self.merges = nn.ModuleList([ConvUnit(cout, cout, 3, 1, 1, relu) for _ in cins[1:]])
 
     def forward(self, xs):
         n = len(xs)
-        xs = [self.outputs[i](xs[i]) for i in range(n)]
+        ys = [self.outputs[i](xs[i]) for i in range(n)]
         for i in range(0, n - 1)[::-1]:
-            xs[i] += F.interpolate(xs[i + 1], size=xs[i].shape[2:], mode='nearest')
-            xs[i] = self.merges[i](xs[i])
-        return xs
+            ys[i] += F.interpolate(ys[i + 1], size=ys[i].shape[2:], mode='nearest')
+            ys[i] = self.merges[i](ys[i])
+        if hasattr(self, 'extra'):
+            ys.append(self.extra(xs[-1]))
+        return ys
 
 
 class SSH(nn.Module):
@@ -67,45 +71,52 @@ class Head(nn.Module):
 
 class RetinaFace(nn.Module):
 
-    def __init__(self, backbone='mobilenet'):
-        super(RetinaFace,self).__init__()
+    def __init__(self, backbone, bases, predict_landmarks):
+        super(RetinaFace, self).__init__()
+        
         if backbone == 'mobilenet':
             self.body = MobileNetV1(0.25, relu_type='lrelu_0.1', return_inter=[5, 11])
-            cins, cout, relu = [64, 128, 256], 64, 'lrelu_0.1'
+            cins, cout, relu, extra = [64, 128, 256], 64, 'lrelu_0.1', False
         elif backbone == 'resnet':
             self.body = ResNet50(return_count=3)
-            cins, cout, relu = [512, 1024, 2048], 256, 'plain'
+            cins, cout, relu, extra = [512, 1024, 2048], 256, 'plain', False
         elif backbone == 'resnet_bbt':
             self.body = ResNet50()
-            cins, cout, relu = [256, 512, 1024, 2048], 256, 'plain'
+            cins, cout, relu, extra = [256, 512, 1024, 2048], 256, 'plain', True
         else:
             raise ValueError('Unknown backbone')
 
-        self.feature_pyramid = FPN(cins, cout, relu)
-        self.context_modules = nn.ModuleList([SSH(cout, cout, relu) for _ in cins])
-        num_anchors = 2
-        self.heads_class = nn.ModuleList([Head(cout, num_anchors, 2) for _ in cins])
-        self.heads_boxes = nn.ModuleList([Head(cout, num_anchors, 4) for _ in cins])
-        self.heads_ldmks = nn.ModuleList([Head(cout, num_anchors, 10) for _ in cins])
+        self.bases = bases
+        num_anchors = len(bases[0][1])
+        num_levels = len(cins) + (1 if extra else 0)
+
+        self.feature_pyramid = FPN(cins, cout, relu, extra)
+        self.context_modules = nn.ModuleList([SSH(cout, cout, relu) for _ in range(num_levels)])
+        self.heads_class = nn.ModuleList([Head(cout, num_anchors, 2) for _ in range(num_levels)])
+        self.heads_boxes = nn.ModuleList([Head(cout, num_anchors, 4) for _ in range(num_levels)])
+        if predict_landmarks:
+            self.heads_ldmks = nn.ModuleList([Head(cout, num_anchors, 10) for _ in range(num_levels)])
 
     def forward(self, imgs):
-        x = cv2.dnn.blobFromImages(imgs, mean=(104, 117, 123), swapRB=False)
-        x = torch.from_numpy(x).to(next(self.parameters()).device)
+        #x = cv2.dnn.blobFromImages(imgs, mean=(104, 117, 123), swapRB=False)
+        #x = cv2.dnn.blobFromImages(imgs, mean=(123, 117, 104), swapRB=True, scalefactor=1/255/0.225)
+        x = np.stack(imgs)
+        x = x[:, :, :, [2, 1, 0]].astype(np.float32) / 255.0
+        x = (x - [0.485, 0.456, 0.406]) / [0.229, 0.224, 0.225]
+        x = x.transpose(0, 3, 1, 2)
+        x = torch.from_numpy(x).to(next(self.parameters()).device, torch.float32)
+        print(x[0][1][5][10:20])
 
         xs = self.body(x)
-
-        from collections import OrderedDict
-        if isinstance(xs, OrderedDict): xs = list(xs.values())
-        
         xs = self.feature_pyramid(xs)
         xs = [self.context_modules[i](xs[i]) for i in range(len(xs))]
         box_reg = torch.cat([self.heads_boxes[i](xs[i]) for i in range(len(xs))], dim=1)
         classif = torch.cat([self.heads_class[i](xs[i]) for i in range(len(xs))], dim=1)
-        ldm_reg = torch.cat([self.heads_ldmks[i](xs[i]) for i in range(len(xs))], dim=1)
+        if hasattr(self, 'heads_ldmks'):
+            ldm_reg = torch.cat([self.heads_ldmks[i](xs[i]) for i in range(len(xs))], dim=1)
         scores = F.softmax(classif, dim=-1)[:, :, 1]
      
-        bases = [(8, [(16, 16), (32, 32)]), (16, [(64, 64), (128, 128)]), (32, [(256, 256), (512, 512)])]
-        priors = get_priors(x.shape[2:], bases).to(x.device)
+        priors = get_priors(x.shape[2:], self.bases).to(x.device)
         boxes = decode_boxes(box_reg, priors)
         l = select_boxes(boxes, scores, score_thr=0.02, iou_thr=0.4)
         return l
@@ -160,9 +171,14 @@ def get_priors(img_size, bases):
     [[16, 16, 8, 4], [16, 16, 25, 15], [48, 16, 8, 4], [48, 16, 25, 15],
      [16, 48, 8, 4], [16, 48, 25, 15], [48, 48, 8, 4], [48, 48, 25, 15],
      [16, 80, 8, 4], [16, 80, 25, 15], [48, 80, 8, 4], [48, 80, 25, 15]]
+
+    In case of square anchors, only one dimension can be provided, i.e. [(8, [16, 32])]
+    will be automatically turned into [(8, [(16, 16), (32, 32)])].
     """
     p = []
     h, w = img_size
+    if isinstance(bases[0][1][0], int):
+        bases = [(s, [(a, a) for a in l]) for (s, l) in bases]
     for stride, anchors in bases:
         nx = math.ceil(w / stride)
         ny = math.ceil(h / stride)
@@ -198,19 +214,29 @@ class RetinaFaceDetector():
         if backbone == 'mobilenet':
             print('Initializing RetinaFace model (with MobileNetV1 backbone) for face detection')
             wf = prep_weights_file('https://drive.google.com/uc?id=15zP8BP-5IvWXWZoYTNdvUJUiBqZ1hxu1', 'mobilenet0.25_Final.pth', gdrive=True)
+            bases = [(8, [(16, 16), (32, 32)]), (16, [(64, 64), (128, 128)]), (32, [(256, 256), (512, 512)])]
+            predict_landmarks = True
         elif backbone == 'resnet':
             print('Initializing RetinaFace model (with ResNet50 backbone) for face detection')
             wf = prep_weights_file('https://drive.google.com/uc?id=14KX6VqF69MdSPk3Tr9PlDYbq7ArpdNUW', 'Resnet50_Final.pth', gdrive=True)
+            bases = [(8, [16, 32]), (16, [64, 128]), (32, [256, 512])]
+            predict_landmarks = True
         elif backbone == 'resnet_bbt':
             print('---BBT---')
             wf = prep_weights_file('https://drive.google.com/uc?id=1uraA7ZdCCmos0QSVR6CJgg0aSLtV4q4m', 'final_mixed_r50.pth', gdrive=True)
+            bases = [(4, [16, 20.16, 25.40]), (8, [32, 40.32, 50.80]), (16, [64, 80.63, 101.59]), (32, [128, 161.26, 203.19]), (64, [256, 322.54, 406.37])]
+            predict_landmarks = False
         
-        self.model = RetinaFace(backbone).to(device)
+        self.model = RetinaFace(backbone, bases, predict_landmarks).to(device)
         #for w in self.model.state_dict(): print(w, '\t', self.model.state_dict()[w].shape)
         wd_src = torch.load(wf, map_location=torch.device(device))
+        
+        if backbone == 'resnet_bbt':
+            for s in ['conv.weight', 'bn.weight', 'bn.bias', 'bn.running_mean', 'bn.running_var', 'bn.num_batches_tracked']: wd_src.pop('fpn.lateral_outs.3.' + s)
         wd_dst = {}
         names = list(wd_src)
         for i, w in enumerate(list(self.model.state_dict())):
+            #print(names[i], ' to ', w)
             wd_dst[w] = wd_src[names[i]]
         self.model.load_state_dict(wd_dst)
         self.model.eval()
